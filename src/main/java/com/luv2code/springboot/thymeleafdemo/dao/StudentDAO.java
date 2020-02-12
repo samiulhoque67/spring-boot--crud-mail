cfc9387aae4309bea5335447b8acc3c76db214c8
samiulhoque67 <samiulhoque67@gmail.com>
2020-02-12 19:43:46 +0600

    first commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/StudentDAO.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Student;
+
+	public interface StudentDAO {
+		public  void saveStudent(Student student); 
+		public List<Student> getStudents() ;
+		public Student get(int theId);
+		
+		public void delete(int theId);
+		public Student mail(int theId);
+		
+			
+		
+}
+	
